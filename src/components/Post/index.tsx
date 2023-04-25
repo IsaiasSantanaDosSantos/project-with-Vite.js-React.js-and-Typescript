2e4fb6a26345f5615fd0bec7d73cf83b6996978c
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2023-04-24 22:24:58 -0300

    useNavigation()

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -9,7 +9,7 @@ export const Post = ()=>{
     const { id } = params
     return (
         <div>
-            <h1>Post {`Params: ${id}`} { `QS: ${qs}`}</h1>
+            <h1>Post {`Params: ${id}`} { `QS: ${qs.get('page')}`} {`segundo: ${qs.get('segundo')}`}</h1>
         </div>
     )
 };
